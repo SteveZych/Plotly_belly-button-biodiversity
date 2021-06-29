55493fc0a3344c4ef7b27fb672225662f8e801c3
Steven Zych <stevezych@gmail.com>
2021-06-28 18:58:11 -0600

    added bar chart to render when id is selected

--- static/js/app.js
+++ static/js/app.js
@@ -1,29 +1,43 @@
 d3.json('data/samples.json').then((data)=>{
     //Create Horizontal bar chart by ID
-    
-    // Function to filter by ID
-    function filterbyID(data){
-        return data.samples.id === "940";
-    }
-    var filteredData = data.filter(filterbyID);
+    console.log(data)
+  
+    var filteredData = data.samples[0];
+    console.log(filteredData);
+  
 
-    var sample_values = filteredData.map((id) => id.sample_values);
+    // Map filtered data, for first ten sample values
+    var sample_values = filteredData.sample_values;
     var barValues = [];
     for (i=0 ; i<10; i++){
         barValues.push(sample_values[i]);
     };
-
-    var otu_ids =  filterData.map((id) => id.otu_ids);
+    console.log(barValues);
+    
+    // Map filtered data for first ten otu_ids
+    var otu_ids =  filteredData.otu_ids;
     var barLabels = [];
     for (i=0 ; i<10; i++){
-        barLabels.push(otu_ids[i]);
+        var label = `OTU ${otu_ids[i]}`
+        barLabels.push(label);
     };
+    console.log(barLabels);
 
-    var otu_labels = filterData.map((id) => id.otu_labels);
+    // Map filtered data for first ten otu_labels
+    var otu_labels = filteredData.otu_labels;
     var barHover = [];
     for (i=0 ; i<10; i++){
         barHover.push(otu_labels[i]);
     };
+    console.log(barHover);
 
-    
+    var trace1 = {
+        x: barValues,
+        y: barLabels,
+        type: "bar",
+        orientation: 'h',
+        text: barHover
+    };
+    var data = [trace1];
+    Plotly.newPlot("bar", data);
 });
